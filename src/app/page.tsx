82fdbe8a99d281c932d7a1d255aaf7facb0b1499
Ville Pynttäri <ville.pynttari@gmail.com>
2024-07-23 17:46:18 +0300

    Testing client side data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
+'use client'
 import ClientRoot from "./components/clientRoot";
 import {getData} from "./fileService";
 import RootLayout from "./layout";
 import MainContent from "@/app/components/mainContent";
-import {CreatorDataType} from "@/app/parserFactory";
 
 export type FileObject = {
     name: string
     download_url: string
 }
 
+export default function Home() {
 
-export default async function Home() {
-
-    let myData: Promise<Partial<CreatorDataType>> = getData()
     return (
         <RootLayout>
             <main className="flex min-h-screen flex-col items-left justify-between p-10">
                 <ClientRoot>
-                    <MainContent myData={myData}/>
+                    <MainContent myData={getData()}/>
                 </ClientRoot>
             </main>
         </RootLayout>
