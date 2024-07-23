8c9567e6f83d1a10f9f81b3d287e32419a405af7
Ville Pynttäri <ville.pynttari@gmail.com>
2024-07-23 18:22:34 +0300

    Testing env file

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "/nextjs-github-pages",
+    basePath: process.env.BASE_URL,
 
     /**
      * Disable server-based image optimization. Next.js does not support
@@ -25,4 +25,5 @@ const nextConfig = {
     },
 };
 
+const baseTrustFactor = process.env.trustFactor
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,20 +3,20 @@ import {Inter} from "next/font/google";
 import "./globals.css";
 import React from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-    <head>
-      <title>Buttercreator</title>
-      <link rel="icon" href="/favicon.svg" sizes="any"/>
-    </head>
-    <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <head>
+            <title>Buttercreator</title>
+            <link rel="icon" href="/favicon.svg" sizes="any"/>
+        </head>
+        <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
