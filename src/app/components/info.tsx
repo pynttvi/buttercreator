20455fb76a8ab4902f867a5e41dce6e0c7c3bb0e
Ville Pynttäri <ville.pynttari@gmail.com>
2024-07-23 23:11:11 +0300

    Fixed wish bonus to stat counter

--- src/app/components/info.tsx
+++ src/app/components/info.tsx
@@ -77,7 +77,7 @@ export default function CharInfo() {
                 }
             )
         })
-    }, [reinc.race, reinc.filteredData, reinc.wishes, reinc.level, reinc.stats])
+    }, [reinc.race, reinc.ready, reinc.wishes, reinc.level, reinc.stats])
 
 
     if (reinc.level === 0 || !reinc.race || !ready) {
