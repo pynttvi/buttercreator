7b9f3239a4f90a79515d96ec8ef79d3667ae8ca6
Ville Pynttäri <ville.pynttari@gmail.com>
2024-07-23 18:22:34 +0300

    Testing env file

--- src/app/components/loadingFallback.tsx
+++ src/app/components/loadingFallback.tsx
@@ -1,22 +1,20 @@
 'use client'
 import {Typography} from "@mui/material";
 import Box from "@mui/material/Box";
-import {styled} from "@mui/material/styles";
-
-const LoadingImage = styled('img')<{
-    width: string;
-    height: string;
-}>(({theme, width, height}) => ({
-    width: width,
-    height: height
-}));
+import Image from "next/image";
 
 export default function LoadingFallback() {
     return (
         <>
             <Typography variant={'h2'}>Loading...</Typography>
             <Box>
-                <LoadingImage src="/favicon.svg" alt="Logo" width={'70%'} height={'70%'}/>
+                <Image
+                    src={`/favicon.svg`}
+                    alt="Vercel Logo"
+                    width={800}
+                    height={800}
+                    priority
+                />
             </Box>
         </>
     )
