033e9d4daf5f5bc44ba3e4f67fea45c4db640bb3
Ville Pynttäri <ville.pynttari@gmail.com>
2024-07-23 18:49:30 +0300

    Fixing image base

--- src/app/components/loadingFallback.tsx
+++ src/app/components/loadingFallback.tsx
@@ -2,14 +2,14 @@
 import {Typography} from "@mui/material";
 import Box from "@mui/material/Box";
 import Image from "next/image";
-
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 export default function LoadingFallback() {
     return (
         <>
             <Typography variant={'h2'}>Loading...</Typography>
             <Box>
                 <Image
-                    src={`/favicon.svg`}
+                    src={`${BASE_URL === "/" ? "" : BASE_URL}favicon.svg`}
                     alt="Vercel Logo"
                     width={800}
                     height={800}
