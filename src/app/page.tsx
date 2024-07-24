81839f5d80d85319dd0311d77cdf6de448d07251
Ville Pynttäri <ville.pynttari@gmail.com>
2024-07-24 08:23:51 +0300

    Optimizing main page loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,46 @@ import ClientRoot from "./components/clientRoot";
 import {getData} from "./fileService";
 import RootLayout from "./layout";
 import MainContent from "@/app/components/mainContent";
+import {useEffect, useState} from "react";
+import {CreatorDataType} from "@/app/parserFactory";
+import LoadingFallback from "@/app/components/loadingFallback";
 
 export type FileObject = {
     name: string
     download_url: string
 }
 
+export function getBaseUrl() {
+    const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+    return BASE_URL === "/" ? "" : BASE_URL
+}
+
 export default function Home() {
+    const [data, setData] = useState<Promise<Partial<CreatorDataType>> | null>(null)
+
+    useEffect(() => {
+        setData(getData())
+    }, []);
+
+
+    if (!data) {
+        return (
+            <RootLayout>
+                <main className="flex min-h-screen flex-col items-left justify-between p-10">
+                    <ClientRoot>
+                        <LoadingFallback/>
+                    </ClientRoot>
+                </main>
+            </RootLayout>
+
+        );
+    }
 
     return (
         <RootLayout>
             <main className="flex min-h-screen flex-col items-left justify-between p-10">
                 <ClientRoot>
-                    <MainContent myData={getData()}/>
+                    <MainContent myData={data}/>
                 </ClientRoot>
             </main>
         </RootLayout>
